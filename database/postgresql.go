3fe35d9d134ce8677f936fc109d7f9b2f2fc5344
Sergey <kot.sergej2012@yandex.ru>
2022-08-23 23:12:11 +0300

    test connection string

--- database/postgresql.go
+++ database/postgresql.go
@@ -1,8 +1,13 @@
 package database
 
 import "gorm.io/gorm"
+import "gorm.io/driver/postgres"
 
 func InitPostgreSql() *gorm.DB {
-
-	return nil
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil
+	}
+	return db
 }
