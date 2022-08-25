c2f82e423d6ba772e401fbfa7d34179b647d4293
Sergey <kot.sergej2012@yandex.ru>
2022-08-26 00:10:36 +0300

    added coments

--- database/postgresql.go
+++ database/postgresql.go
@@ -9,18 +9,20 @@ import (
 )
 
 func InitPostgresql() (*gorm.DB, error) {
+	// creating connection string
 	connString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
 		viper.Get("postgresql.user"),
 		viper.Get("postgresql.pass"),
 		viper.Get("postgresql.host"),
 		viper.Get("postgresql.port"),
 		viper.Get("postgresql.db"))
-	fmt.Printf("--> Connecting to:%s\n", connString)
 
+	// connecting
 	db, err := gorm.Open(postgres.Open(connString), &gorm.Config{})
 	if err != nil {
-		return nil, err
+		return nil, errors.New("error while connecting to database:" + err.Error())
 	}
+	// creating tables from code models
 	err = db.AutoMigrate(&User{}, &Chat{}, &Message{})
 	if err != nil {
 		return nil, errors.New("error migrating database:" + err.Error())
