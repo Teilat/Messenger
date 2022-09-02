40fa155e91a172ac0121a321c1953135af1a22c3
Sergey <kot.sergej2012@yandex.ru>
2022-09-02 11:47:36 +0300

    updated models

--- database/models.go
+++ database/models.go
@@ -6,22 +6,23 @@ import (
 )
 
 type User struct {
-	Id          uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Username    string    `gorm:"index:,unique"` // starts with @
-	Name        string
-	Description string
-	PwHash      string
-	CreatedAt   time.Time
-	LastOnline  time.Time
-	Image       []byte
-	Chats       []*Chat   `gorm:"many2many:user_chats;"` // many to many
-	Messages    []Message // one to many
+	Id         uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Username   string    `gorm:"index:,unique"` // starts with @
+	Name       string
+	Bio        string
+	Phone      string
+	PwHash     string
+	CreatedAt  time.Time `gorm:"default:(now() at time zone 'msk')"`
+	LastOnline time.Time `gorm:"default:(now() at time zone 'msk')"`
+	Image      []byte
+	Chats      []*Chat   `gorm:"many2many:user_chats;"` // many to many
+	Messages   []Message // one to many
 }
 
 type Chat struct {
 	Id           uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Name         string
-	CreationDate time.Time
+	CreationDate time.Time `gorm:"default:(now() at time zone 'msk')"`
 	Users        []*User   `gorm:"many2many:user_chats;"` // many to many
 	Messages     []Message // one to many
 }
@@ -29,7 +30,7 @@ type Chat struct {
 type Message struct {
 	Id        uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Text      string
-	CreatedAt time.Time
+	CreatedAt time.Time `gorm:"default:(now() at time zone 'msk')"`
 	EditedAt  time.Time
 	DeletedAt time.Time
 	UserId    uuid.UUID // one to many user id
